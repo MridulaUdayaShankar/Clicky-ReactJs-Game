fc335ad0d78218320e227c2442597232eef79873
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-24 19:10:56 -0700

    Create Main component

---
+++ clickygame/src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => (
+    <div className="main">
+        <Container>
+        </Container>
+    </div>
+);
+
+
+export default Main;
